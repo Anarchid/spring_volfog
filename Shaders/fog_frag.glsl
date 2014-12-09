35d686541ff1aa35a94bf2fa0302df5cc867168b
Anarchid <anarchid@mail.ru>
2014-12-09 22:52:53 +0200

    spec compatibility, minor cleanup

--- Shaders/fog_frag.glsl
+++ Shaders/fog_frag.glsl
@@ -1,13 +1,13 @@
 const float fogAtten  = %f;
 const float fogHeight = %f;
 const vec3 fogColor   = vec3(%f, %f, %f);
-const int mapX = int(%f);
-const int mapZ = int(%f);
+const float mapX = float(%f);
+const float mapZ = float(%f);
 const float k = 100.0;
-const vec3 up = vec3(0.,1.,0.);
-const vec4 nullVector = (0.,0.,0.,0.);
-const float fogMinHeight = %f;
-const float noiseScale = 256;
+const vec3 up = vec3(0.0,1.0,0.0);
+const vec4 nullVector = vec4(0.0,0.0,0.0,0.0);
+const float fogMinHeight = float(%f);
+const float noiseScale = 256.0;
 
 uniform sampler2D tex0;
 uniform sampler2D tex1;
@@ -44,10 +44,10 @@ float inPrism(in vec3 pos){
 	float(
 		pos.y < fogHeight &&
 		pos.y > fogMinHeight &&
-		pos.z < mapZ-1 &&
-		pos.x < mapX-1 &&
-		pos.x > 1 &&
-		pos.z > 1
+		pos.z < mapZ-1.0 &&
+		pos.x < mapX-1.0 &&
+		pos.x > 1.0 &&
+		pos.z > 1.0
 	);
 }
 
@@ -55,8 +55,8 @@ vec4 mapClouds( in vec3 p)
 {
     float f;
     
-    float factor = 1-smoothstep(fogHeight-20.,fogHeight,p.y);
-    factor = mix(0,factor,inPrism(p));
+    float factor = 1.0-smoothstep(fogHeight-20.0,fogHeight,p.y);
+    factor = mix(0.0,factor,inPrism(p));
     //factor *= 1-pow(p.y/fogHeight,2);
     
     p += offset;
@@ -67,22 +67,9 @@ vec4 mapClouds( in vec3 p)
     f += 0.1250*noise( p ); p = m*p*2.01;
     f += 0.0625*noise( p );
     
+    f = f*factor;
     
-    return f*factor;
-}
-
-vec4 mapDebug(in vec3 pos){
-	float factor = inPrism(pos);
-	pos += offset;
-	vec3 debugColor = factor * pow(2.0 * abs(0.5 - fract(pos / k)), 6.0);
-	return vec4(debugColor, length(debugColor));
-}
-
-vec4 mapTexture(in vec3 pos){
-	float factor = inPrism(pos);
-	vec3 debugColor = texture2D(tex1, vec2(pos.x/mapX,pos.z/mapZ));
-
-	return vec4(debugColor, length(debugColor));
+    return vec4(f,f,f,f);
 }
 
 vec3 planeIntersect(in vec3 startPos, in vec3 endPos, in vec3 planeNormal, in vec3 planeOrigin)
@@ -95,7 +82,7 @@ vec3 planeIntersect(in vec3 startPos, in vec3 endPos, in vec3 planeNormal, in ve
 vec3 cullEndpoint(in vec3 startPos, in vec3 endPos){
 		vec3 cullPos = endPos;
 	
-		if(cullPos.x < 0){
+		if(cullPos.x < 0.0){
 			cullPos =  planeIntersect(startPos, cullPos, vec3(1.,0.,0.), vec3(0.,0.,0.)); 
 		}
 		
@@ -103,7 +90,7 @@ vec3 cullEndpoint(in vec3 startPos, in vec3 endPos){
 			cullPos =  planeIntersect(startPos, cullPos, vec3(1.,0.,0.), vec3(mapX,0.,0.)); 
 		}
 		
-		if(cullPos.z < 0){
+		if(cullPos.z < 0.0){
 			cullPos = planeIntersect(startPos, cullPos, vec3(0.,0.,1.), vec3(0.,0.,0.)); 
 		}
 		
@@ -124,7 +111,7 @@ vec3 cullStartPoint(in vec3 startPos, in vec3 endPos){
 			startPos = planeIntersect(startPos, endPos, up, vec3(0.,fogHeight,0.));
 		}
 
-		if(startPos.x < 0){
+		if(startPos.x < 0.0){
 			startPos = planeIntersect(startPos, endPos, vec3(1.,0.,0.), vec3(0.,0.,0.)); 
 		}
 		
@@ -132,7 +119,7 @@ vec3 cullStartPoint(in vec3 startPos, in vec3 endPos){
 			startPos = planeIntersect(startPos, endPos, vec3(1.,0.,0.), vec3(mapX,0.,0.)); 
 		}
 		
-		if(startPos.z < 0){
+		if(startPos.z < 0.0){
 			startPos = planeIntersect(startPos, endPos, vec3(0.,0.,1.), vec3(0.,0.,0.)); 
 		}
 		
@@ -149,26 +136,26 @@ vec4 raymarchClouds( in vec3 start, in vec3 end)
 	
 	vec3 sectPos = cullStartPoint(start, end);
 	
-	float depth = clamp(sqrt(length(end-sectPos)/1000.),0,1);
+	float depth = clamp(sqrt(length(end-sectPos)/1000.0),0.0,1.0);
 	
 	vec3 rd = normalize(start-end);
 
 	for(int i=0; i<20; i++) // 64 steps maximum
 	{
 		if( sum.w>0.99 ) break; // short-circuit on full opacity or timeout?
-		float t = i/20.0;
+		float t = float(i)/20.0;
 		vec3 pos = mix(sectPos, end, t);
 		vec4 col = mapClouds(pos);
 
 		
-		vec3 lightPos = 10*sundir+pos;
-		float dif =  clamp((col.w - mapClouds(lightPos).w), 0.0, 1.0 )*3;
+		vec3 lightPos = 10.0*sundir+pos;
+		float dif =  clamp((col.w - mapClouds(lightPos).w), 0.0, 1.0 )*3.0;
 
         vec3 lin = vec3(0.76,0.68,0.88)*1.35 + suncolor*dif;
 		col.xyz *= lin;
 		
 		
-		col.a *= 0.6;
+		col.a *= 0.3;
 		col.rgb *= col.a;
 
 		sum = col*(1.0 - sum.a)*depth + sum;	
